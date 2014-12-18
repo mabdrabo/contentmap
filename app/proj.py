08f1aed4ced67370f56e0b3d4d9aac653e494bca
Mahmoud Abdrabo <abdrabo.mahmoud@gmail.com>
2014-12-18 15:59:18 +0200

    fetch all countries with links from alexa

--- app/proj.py
+++ app/proj.py
@@ -11,13 +11,25 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def home():
+	import requests
+	from lxml import html
+	url = "http://www.alexa.com/topsites/countries"
+	page = requests.get(url)
+	tree = html.fromstring(page.text)
+	lis = tree.xpath('//div[@class="categories top"]//li//a')
+	countries = [{l.text: l.items()[0][1]} for l in lis]
 	return render_template('home.html')
 
 
-@app.route('/country', methods=['POST'])
+@app.route('/country')
 def country():
-	country = request.form['country']
 	return render_template('country.html')
+
+
+@app.route('/country_submit', methods=['POST'])
+def country_submit():
+	country = request.form['country']
+	return render_template('report.html', country=country)
 
 if __name__ == '__main__':
 	app.run(debug=True)
